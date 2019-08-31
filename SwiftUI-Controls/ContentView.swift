ed78a483ef200c6a9355975f8d936dd5780ea65c
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-08-31 11:00:22 -0400

    Initial Commit

---
+++ SwiftUI-Controls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Controls
+//
+//  Created by Tommie Carter on 8/31/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
